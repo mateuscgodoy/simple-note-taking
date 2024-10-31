2821bbc96a84cbced7a3dc8b981264781e720127
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-10-31 10:08:46 -0300

    feat: add the Prompt service to the ICommand interface

--- src/commands/CreateCommand.ts
+++ src/commands/CreateCommand.ts
@@ -1,9 +1,7 @@
-import { input } from '@inquirer/prompts';
-
 import NoteService, { OperationResult } from '../services/NoteService.js';
-import validateStringLength from '../utils/validateStringInput.js';
 import ICommand from './ICommand.js';
 import { OPS } from '../models/OPS.js';
+import PromptService from '../services/PromptService.js';
 
 export default class CreateCommand implements ICommand {
   private _operation: OPS = OPS.CREATE;
@@ -12,17 +10,12 @@ export default class CreateCommand implements ICommand {
     return this._operation;
   }
 
-  async execute(noteService: NoteService): Promise<OperationResult> {
-    const displayTitle = await input({
-      message: 'Enter the title of your note:',
-      required: true,
-      validate: (value) => validateStringLength(value),
-    });
-    const content = await input({
-      message: 'Enter the content of your note (max. 500 characters):',
-      required: true,
-      validate: (value) => validateStringLength(value, 500),
-    });
+  async execute(
+    noteService: NoteService,
+    promptService: PromptService
+  ): Promise<OperationResult> {
+    const displayTitle = await promptService.getTitle();
+    const content = await promptService.getContent();
     return await noteService.createNote(displayTitle, content);
   }
 }
--- src/commands/ICommand.ts
+++ src/commands/ICommand.ts
@@ -1,7 +1,11 @@
 import { OPS } from '../models/OPS.js';
 import NoteService, { OperationResult } from '../services/NoteService.js';
+import PromptService from '../services/PromptService.js';
 
 export default interface ICommand {
   operation: OPS;
-  execute(noteService: NoteService): OperationResult | Promise<OperationResult>;
+  execute(
+    noteService: NoteService,
+    promptService: PromptService
+  ): OperationResult | Promise<OperationResult>;
 }
